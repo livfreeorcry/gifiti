fefa52550c7d28079704abf5daa9c5274b850a44
James <James@armifi.com>
2016-07-22 00:27:42 -0700

    Search term functionality

--- gifitti.py
+++ gifitti.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from gif import gif
 
 app = Flask(__name__)
@@ -7,9 +7,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    imglink=gif("pepe")
-    response="""{{
-    "response_type": "in_channel",
-    text:"{0}"
-}}"""
-    return response.format( imglink )
+    term=request.args.get('text')
+    imglink=gif(term)
+    response = { "response_type" : "in_channel", "text" : imglink}
+    if len(imglink) > 5:
+        return jsonify(**response)
+    else:
+        return "No gif found :("
